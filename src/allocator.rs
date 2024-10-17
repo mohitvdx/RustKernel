1bbd808f93949ce8f8f5e4cf6f30847249feb559
Mohit Verma <112820522+mohitvdx@users.noreply.github.com>
2024-10-17 15:45:00 +0530

    add linked list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -2,15 +2,18 @@ use alloc::alloc::{GlobalAlloc, Layout};
 use bump::BumpAllocator;
 use core::ptr::null_mut;
 use linked_list_allocator::LockedHeap;
+use linked_list::LinkedListAllocator;
 
 pub mod bump;
+pub mod linked_list;
 
 pub struct Dummy;
 pub const HEAP_START: usize = 0x4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> =
+    Locked::new(LinkedListAllocator::new());
 
 pub struct Locked<A> {
     inner: spin::Mutex<A>,
@@ -46,31 +49,31 @@ use x86_64::{
     VirtAddr,
 };
 
-pub fn init_heap(
-    mapper: &mut impl Mapper<Size4KiB>,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) -> Result<(), MapToError<Size4KiB>> {
-    let page_range = {
-        let heap_start = VirtAddr::new(HEAP_START as u64);
-        let heap_end = heap_start + HEAP_SIZE - 1u64;
-        let heap_start_page = Page::containing_address(heap_start);
-        let heap_end_page = Page::containing_address(heap_end);
-        Page::range_inclusive(heap_start_page, heap_end_page)
-    };
+// pub fn init_heap(
+//     mapper: &mut impl Mapper<Size4KiB>,
+//     frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+// ) -> Result<(), MapToError<Size4KiB>> {
+//     let page_range = {
+//         let heap_start = VirtAddr::new(HEAP_START as u64);
+//         let heap_end = heap_start + HEAP_SIZE - 1u64;
+//         let heap_start_page = Page::containing_address(heap_start);
+//         let heap_end_page = Page::containing_address(heap_end);
+//         Page::range_inclusive(heap_start_page, heap_end_page)
+//     };
 
-    for page in page_range {
-        let frame = frame_allocator
-            .allocate_frame()
-            .ok_or(MapToError::FrameAllocationFailed)?;
-        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
-        unsafe {
-            ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
-            mapper.map_to(page, frame, flags, frame_allocator)?.flush()
-        };
-    }
+//     for page in page_range {
+//         let frame = frame_allocator
+//             .allocate_frame()
+//             .ok_or(MapToError::FrameAllocationFailed)?;
+//         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+//         unsafe {
+//             ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE);
+//             mapper.map_to(page, frame, flags, frame_allocator)?.flush()
+//         };
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
 
 fn align_up(addr: usize, align: usize) -> usize {
     (addr + align - 1) & !(align - 1)
